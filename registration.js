680065050e80a3e82317ef7cdba9e2dd4f663702
daftarteraonline <pendataanUTTP.ska@gmail.com>
2023-11-13 15:35:51 +0700

    membuat fitur pendaftaran dengan qrcode 28

--- registration.js
+++ registration.js
@@ -8,4 +8,16 @@ function changeDate() {
   }
 }
 
+function getNowDate() {
+  let d = new Date();
+  let nowDate = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
+  //console.log(nowDate);
+  for (let k of document.querySelectorAll('.tgl')) {
+    k.value = nowDate;
+  }
+}
+
+getNowDate();
 changeDate();
+
+
