860f347d4e80db6ed77e60e6498a2e81ab3eef9c
belajarwebdevelopment <belajar.webdevelopment@gmail.com>
2023-11-16 04:58:38 +0700

    membuat fitur pendaftaran dengan qrcode 29

--- registration.js
+++ registration.js
@@ -2,7 +2,7 @@ function changeDate() {
   let tgl = document.querySelectorAll('.tgl');
   for (let k of tgl) {
     k.addEventListener("change", () => {
-      alert(k.id);
+      alert(k.value);
     });
 
   }
@@ -11,7 +11,7 @@ function changeDate() {
 function getNowDate() {
   let d = new Date();
   let nowDate = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`;
-  //console.log(nowDate);
+  
   for (let k of document.querySelectorAll('.tgl')) {
     k.value = nowDate;
   }
