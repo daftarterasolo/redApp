03f3bf1a8e0e93d959768328dd21eb4c3a7ee6de
zorin <belajar.webdevelopment@gmail.com>
2024-03-07 16:24:59 +0700

    mencoba 20

---
+++ ujiApi.js
@@ -0,0 +1,40 @@
+const downloadBuktiDaftar = () => {
+	let api = "http://192.168.100.106:5005/buktidaftar";
+
+	let data = {"bukti_daftar": [["2024-01-16T03:23:27.319Z",114,"PAKET HERONA EXPRESS","Kestalan, Surakarta","-","62","TS","500 kg","200 g","PRESISIE","E160029","-",1,"Indonesia","gc","","114/TS/TUK/I/2024",225]]}
+
+	fetch(api,{
+		method : 'POST',
+		headers : {
+			"Content-Type" : "application/json"
+		},
+		data : JSON.stringify(data)
+	})
+	.then(result => result.json())
+	.then(result => {
+		console.log(result);
+	});
+
+
+/*
+	fetch(api, {
+		method : 'GET',
+		headers : {
+			"Accept" : "Application/octet-stream"
+		}
+	})
+	.then(data => data.blob())
+	.then(data => {
+		const aElement = document.createElement("a");
+		aElement.setAttribute("download", "bukti_daftar_terbaru_114_04_Maret_2024.docx");
+		const href = URL.createObjectURL(data);
+		aElement.href = href;
+		aElement.setAttribute("target", "_blank");
+		aElement.click();
+		URL.revokeObjectURL(href);
+	});
+*/
+}
+
+
+downloadBuktiDaftar();
