f316cba4ecd4e4fc0fc53b79b505fe20a2531cfd
zorin <belajar.webdevelopment@gmail.com>
2024-03-14 08:55:40 +0700

    mencoba 23

--- ujiApi.js
+++ ujiApi.js
@@ -1,6 +1,6 @@
 const downloadBuktiDaftar = () => {
 	//let api = "http://192.168.100.106:5005/buktidaftar";
-	let api = "http://sert.metrologi.ska/buktidaftar";
+	let api = "https://sert.metrologi.ska:5005/buktidaftar";
 
 	let data = {"bukti_daftar": [["2024-01-16T03:23:27.319Z",114,"PAKET HERONA EXPRESS","Kestalan, Surakarta","-","62","TS","500 kg","200 g","PRESISIE","E160029","-",1,"Indonesia","gc","","114/TS/TUK/I/2024",225]]}
 
@@ -9,7 +9,7 @@ const downloadBuktiDaftar = () => {
 		headers : {
 			"Content-Type" : "application/json"
 		},
-		data : JSON.stringify(data)
+		body : JSON.stringify(data)
 	})
 	.then(result => result.json())
 	.then(result => {
